0ddfd8f05909304848a32f7c71af235b8441d71d
Antsega <astrowelding@gmail.com>
2023-03-28 00:50:48 -0500

    everything working but getting calendar API error

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 
 import './App.css';
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+import DateTimePicker from 'react-datetime-picker';
+import { useState } from 'react';
 
 function App() {
+  const [ start, setStart ] = useState(new Date())
+  const [ end, setEnd ] = useState(new Date())
+  const [ eventName, setEventName ] = useState("");
+  const [ eventDescription, setEventDescription ] = useState("")
+
+
   const session = useSession(); // tokens
   const supabase = useSupabaseClient(); // talk to supabase
+  const { isLoading } = useSessionContext(); 
+
+  if(isLoading) {
+    return <></>
+  }
 
   async function googleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        scopes: 'https://www.googleapis.com/auth/calendar' // add a space and can add multiple providers like google drive ex "link link" 
+        scopes: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events' // add a space and can add multiple providers like google drive ex "link link" 
       }
     });
     if(error) {
@@ -23,7 +36,38 @@ function App() {
     await supabase.auth.signOut();
   }
 
+  async function createCalendarEvent() {
+    console.log("Creating calendar event")
+    const event = {
+      "summary" : eventName,
+      "description" : eventDescription,
+      'start' : {
+        'dateTime' : start.toISOString(), // Date.toISOString() -> comunicate with google
+        'timeZone' : Intl.DateTimeFormat().resolvedOptions().timeZone // get user timezone
+      },
+      'end' : {
+        'dateTime' : end.toISOString(), 
+        'timeZone' : Intl.DateTimeFormat().resolvedOptions().timeZone // get user timezone
+      }
+    }
+    await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+      method: "POST" ,
+      headers: {
+        'Authorization' : 'Bearer ' + session.provider_token // Access token for google
+      },
+      body: JSON.stringify(event)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      alert("Event created, check your Google Calendar!");
+    })
+  }
+
 console.log(session);
+console.log(start);
+console.log(eventName);
+console.log(eventDescription);
 
   return (
     <div className="App">
@@ -31,6 +75,21 @@ console.log(session);
         {session ?
         <>
           <h2>Hey there {session.user.email}</h2>
+          <p>Start of your event</p>
+          <DateTimePicker onChange={setStart} value={start}></DateTimePicker>
+
+          <p>End of your event</p>
+          <DateTimePicker onChange={setStart} value={end}></DateTimePicker>
+
+          <p>Event Name</p>
+          <input type="text" onChange={(e) => setEventName(e.target.value)} />
+
+          <p>Event Description</p>
+          <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
+          <hr />
+          <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
+          <p></p>
+
           <button onClick={() => signOut()}>Sign Out</button>
         </>
         :
